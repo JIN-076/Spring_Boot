bf81fb9935a62eca6e1f8843e3deb53fc20f3ec5
JIN-076 <andy230@naver.com>
2022-02-07 23:50:04 +0900

    [Spring Boot] Spring Principle

---
+++ core/src/main/java/spring/core/AppConfig.java
@@ -0,0 +1,31 @@
+package spring.core;
+
+import spring.core.discount.DiscountPolicy;
+import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
+import spring.core.member.Member;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+import spring.core.member.MemoryMemberRepository;
+import spring.core.order.Order;
+import spring.core.order.OrderService;
+import spring.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
---
+++ core/src/test/java/spring/core/member/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package spring.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spring.core.AppConfig;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join() {
+        // given : 이러한 상황이 주어졌을 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when : 이 타이밍에
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then : 이러한 동작을 수행할거야
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
